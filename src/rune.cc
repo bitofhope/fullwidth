979b0cdf580e45c0cb301d8a02e8982f579480fd
Bradley Kennedy <bk@co60.ca>
2017-02-25 21:14:07 -0500

    Banners

--- src/rune.cc
+++ src/rune.cc
@@ -1,4 +1,23 @@
-
+/*
+* bunnysay
+*
+*  Created on: 2015-11-30
+*      Author: brad
+*
+*   This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 #include "rune.h"
 
 Rune::Rune(uint32_t v) : d(v) {
